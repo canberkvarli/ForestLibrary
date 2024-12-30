f3be70e2edd851adb49ab2df591db329d9baab94
Canberk Varli <canberkvarli@gmail.com>
2024-12-30 01:35:31 -0800

    add music

--- frontend/src/components/nav/navbar.js
+++ frontend/src/components/nav/navbar.js
@@ -1,31 +1,28 @@
-import React from 'react';
-import { withRouter } from 'react-router-dom'
-import Modal from '../modal/modal'
-import './navbar.css'
-import song from '../../assets/audioFiles/Gotama - Inner Sanctuary.mp3'
+import React from "react";
+import { withRouter } from "react-router-dom";
+import Modal from "../modal/modal";
+import "./navbar.css";
+import song from "../../assets/audioFiles/Gotama - Inner Sanctuary.mp3";
 
 class NavBar extends React.Component {
   constructor(props) {
     super(props);
-    
+
     this.state = {
       show: false,
       playing: false,
-      musicBgColor: '',
-      audio: new Audio(song)
+      musicBgColor: "",
+      audio: new Audio(song),
     };
 
     this.logoutUser = this.logoutUser.bind(this);
     this.getLinks = this.getLinks.bind(this);
     this.showModal = this.showModal.bind(this);
     this.hideModal = this.hideModal.bind(this);
-    this.getLinks = this.getLinks.bind(this); 
-
-    
+    this.getLinks = this.getLinks.bind(this);
   }
-  
- playPause = () => {
 
+  playPause = () => {
     // Get state of song
     let playing = this.state.playing;
 
@@ -34,15 +31,14 @@ class NavBar extends React.Component {
 
       this.state.audio.pause();
       this.setState({
-        musicBgColor: "lightgray"
-      })
+        musicBgColor: "lightgray",
+      });
     } else {
-
       // Play the song if it is paused
       this.state.audio.play();
       this.setState({
-        musicBgColor: "orangered"
-      })
+        musicBgColor: "orangered",
+      });
     }
 
     // Change the state of song
@@ -52,7 +48,7 @@ class NavBar extends React.Component {
   // }
 
   // componentWillUnmount() {
-  //   audio.removeEventListener('ended', () => this.setState({ play: false }));  
+  //   audio.removeEventListener('ended', () => this.setState({ play: false }));
   // }
 
   // togglePlay = () => {
@@ -61,73 +57,87 @@ class NavBar extends React.Component {
   //   });
   // }
 
-  showModal(){
+  showModal() {
     this.setState({
-      modal: true
-    })
+      modal: true,
+    });
   }
 
-  hideModal(){
+  hideModal() {
     this.setState({
-      modal: false
-    })
+      modal: false,
+    });
   }
 
   logoutUser(e) {
-      e.preventDefault();
-      this.props.logout();
+    e.preventDefault();
+    this.props.logout();
   }
 
-  handleClick(route){
+  handleClick(route) {
     return (e) => this.props.history.push(route);
   }
-  
-  demoLogin(e){
+
+  demoLogin(e) {
     e.preventDefault();
-    this.props.login({username:'demo',password:'123456'});//.then(this.props.history.push('/users/6148f9737c929f02f3991ffe'));
+    this.props.login({ username: "demo", password: "123456" }); //.then(this.props.history.push('/users/6148f9737c929f02f3991ffe'));
   }
   // Selectively render links dependent on whether the user is logged in
   getLinks() {
-      if (this.props.loggedIn) {
-        return (
-            <div className='links-container'>
-                <div onClick={this.handleClick(`/users/${this.props.session.user.id}/profile`).bind(this)}>My Tree</div>
-   
-                <div onClick={this.handleClick('/').bind(this)}>Home</div>
-                <div style={{borderColor: this.state.musicBgColor}}>
-                  <img id="music-icon" src="https://image.flaticon.com/icons/png/16/898/898792.png" onClick={this.playPause}/>
-                </div>
-                <div onClick={this.logoutUser}>Logout</div>
-            </div>
-        );
-      } else {
-        return (
-            <div className='links-container'>
-                <div onClick={this.demoLogin.bind(this)}>Demo</div>
-                <div onClick={this.handleClick('/register').bind(this)}>Signup</div>
-                <div onClick={this.handleClick('/login').bind(this)}>Login</div>
-            </div>
-        );
-      }
+    if (this.props.loggedIn) {
+      return (
+        <div className="links-container">
+          <div
+            onClick={this.handleClick(
+              `/users/${this.props.session.user.id}/profile`
+            ).bind(this)}
+          >
+            My Tree
+          </div>
+
+          <div onClick={this.handleClick("/").bind(this)}>Home</div>
+          <div style={{ borderColor: this.state.musicBgColor }}>
+            <img
+              id="music-icon"
+              src="https://image.flaticon.com/icons/png/16/898/898792.png"
+              onClick={this.playPause}
+            />
+          </div>
+          <div onClick={this.logoutUser}>Logout</div>
+        </div>
+      );
+    } else {
+      return (
+        <div className="links-container">
+          <div onClick={this.demoLogin.bind(this)}>Demo</div>
+          <div onClick={this.handleClick("/register").bind(this)}>Signup</div>
+          <div onClick={this.handleClick("/login").bind(this)}>Login</div>
+        </div>
+      );
+    }
   }
 
   render() {
-      return (
-        <div className='nav-bar-container'>
-          
-            <div className='nav-bar-logo' onClick={this.handleClick('/').bind(this)}>Forest Library</div>
-
-              <Modal show={this.state.modal} handleClose={this.hideModal} />
-                            <div 
-                className="nav-icons" 
-                id="instructions-icon"
-                >
-                      <img src="https://image.flaticon.com/icons/png/64/4293/4293075.png" id="img-instructions" onClick={this.showModal}/>
-                </div>
-            { this.getLinks() }
-            
+    return (
+      <div className="nav-bar-container">
+        <div
+          className="nav-bar-logo"
+          onClick={this.handleClick("/").bind(this)}
+        >
+          Forest Library
         </div>
-      );
+
+        <Modal show={this.state.modal} handleClose={this.hideModal} />
+        <div className="nav-icons" id="instructions-icon">
+          <img
+            src="https://image.flaticon.com/icons/png/64/4293/4293075.png"
+            id="img-instructions"
+            onClick={this.showModal}
+          />
+        </div>
+        {this.getLinks()}
+      </div>
+    );
   }
 }
 
